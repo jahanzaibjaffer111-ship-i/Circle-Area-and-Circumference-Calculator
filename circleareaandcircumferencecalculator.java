c9c4599f5ca83283e966d32c57fd00d3391b7d1d
Jahanzaib Jaffer Ali <jahanzaib.jaffer111@gmail.com>
2026-01-12 12:07:39 +0500

    Add files via upload

---
+++ circleareaandcircumferencecalculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class circleareaandcircumferencecalculator
+{
+    public static void main(String[] args)
+    {
+        /*a=radius
+        b=area
+        c=circumference */
+        double a,b,c;
+        try (Scanner read = new Scanner(System.in)) {
+            while(1>0)
+            {
+                System.out.println("HELLO");
+                System.out.println("WELCOME TO AREA AND CIRCUMFERENCE CALCULATOR");
+                System.out.println("ENTRE RADIUS OF CIRCLE");
+                a=read.nextDouble();
+                if (a>0)
+                {
+                    b=(22* Math.pow(a, 2))/7;
+                    c=(2*22*a)/7;
+                    System.out.println("AREA OF CIRCLE IS");
+                    System.out.println(b);
+                    System.out.println("CIRCUMFERENCE OF CIRCLE IS");
+                    System.out.println(c);
+                    System.out.println("THANKS");
+                    System.out.println("BYE");
+                    System.out.println(".");
+
+                }
+                if (a<0)
+                {
+                    System.out.println("RADIUS CANT BE NEGATIVE");
+                    System.out.println("TRY AGAIN");
+                    System.out.println(".");
+                }
+            }
+        }
+
+    }
+}
